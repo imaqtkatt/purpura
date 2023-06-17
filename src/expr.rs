486f9f547565bafec9673a79fabf80334bca462f
imaqtkatt <eduarda.so@proton.me>
2023-06-17 14:13:10 -0300

    feat: Operator enum
    - parsing number as usize
    - parsing sum operation

--- src/expr.rs
+++ src/expr.rs
@@ -3,4 +3,14 @@
 pub enum Expr {
     Number(usize),
     Identifier(String),
+    BiOp(Operator, Box<Expr>, Box<Expr>)
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Operator {
+    Mul,
+    Div,
+    Sum,
+    Min,
+
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,6 @@
-use crate::{lexer::Lexer, token::Token, expr::Expr};
+use std::array::from_ref;
+
+use crate::{lexer::Lexer, token::Token, expr::{Expr, Operator}};
 
 pub struct Parser<'a> {
     lexer: Lexer<'a>,
@@ -35,15 +37,32 @@ impl<'a> Parser<'a> {
     }
 
     fn parse_literal(&mut self) -> Result<Expr, String> {
-        match self.current.as_ref() {
+        let literal = match self.current.as_ref() {
             Some(Token::Identifier(s)) => {
                 Ok(Expr::Identifier(String::clone(s)))
             },
-            Some(Token::Number(_n)) => {
-                Ok(Expr::Number(0))
+            Some(Token::Number(string_number)) => {
+                match string_number.parse::<usize>() {
+                    Ok(num) => Ok(Expr::Number(num)),
+                    _ => Err(format!("Could not read {:?} as number", string_number))
+                }
             }
             _ => panic!("Expected a literal")
+        };
+        self.advance();
+        literal
+    }
+
+    fn parse_bi_op(&mut self) -> Result<Expr, String> {
+        let mut left = self.parse_literal()?;
+
+        while self.current == Some(Token::Plus) {
+            self.advance();
+            let right = self.parse_literal()?;
+            left = Expr::BiOp(Operator::Sum, Box::new(left), Box::new(right));
         }
+
+        Ok(left)
     }
 }
 
@@ -51,7 +70,7 @@ impl<'a> Parser<'a> {
 mod test {
     use std::io::Result;
 
-    use crate::expr::Expr;
+    use crate::expr::{Expr, Operator};
 
     use super::Parser;
 
@@ -76,9 +95,27 @@ mod test {
         let mut parser = Parser::new(&source);
         let number_literal = parser.parse_literal();
 
-        let expected = Ok(Expr::Number(0));
+        let expected = Ok(Expr::Number(42));
 
         assert_eq!(number_literal, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn parse_sum_bi_op() -> Result<()> {
+        let source: String = "0 + 1".into();
+
+        let mut parser = Parser::new(&source);
+        let bi_op = parser.parse_bi_op();
+
+        let expected = Expr::BiOp(
+            Operator::Sum,
+            Box::new(Expr::Number(0)),
+            Box::new(Expr::Number(1))
+        );
+
+        assert_eq!(bi_op, Ok(expected));
 
         Ok(())
     }
