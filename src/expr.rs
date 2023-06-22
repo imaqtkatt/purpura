18a838dc67f32ce0f4ce8ce10285c79ffed2fc44
imaqtkatt <eduarda.so@proton.me>
2023-06-22 15:01:53 -0300

    feat: statements
    Expr: add Match and Block variants
    
    Add Statement enum
    
    Rename Pattern struct to Arm
    
    Add Type, FnBody and Pattern enums
    
    Add Signature, Constructor, Data and Fn structs

--- src/expr.rs
+++ src/expr.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug)]
 pub enum Expr {
     Number(u64),
     String(String),
@@ -6,23 +6,76 @@ pub enum Expr {
     Binary(Operator, Box<Expr>, Box<Expr>),
     Lambda(String, Box<Expr>),
     Application(Box<Expr>, Vec<Expr>),
-    FnDefinition(String, Vec<Expr>, Box<Expr>),
-    Match(Box<Expr>, Vec<Pattern>),
+    Match(Box<Expr>, Vec<Arm>),
+    Block(Vec<Statement>),
 }
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct Pattern {
-    pub left: Box<Expr>,
+#[derive(Debug)]
+pub enum Statement {
+    Expr(Box<Expr>),
+    Let(String, Expr),
+}
+
+#[derive(Debug)]
+pub struct Arm {
+    pub left: Box<Pattern>,
     pub right: Box<Expr>,
 }
 
-impl Pattern {
-    pub fn new(left: Expr, right: Expr) -> Self {
+impl Arm {
+    pub fn new(left: Pattern, right: Expr) -> Self {
         Self {
             left: Box::new(left),
             right: Box::new(right),
         }
     }
+}
+
+#[derive(Debug)]
+pub enum Type {
+    Identifier(String),
+    Generic(String, Vec<Type>),
+    TypeVariable(String),
+}
+
+#[derive(Debug)]
+pub enum FnBody {
+    Block(Vec<Statement>),
+    Expr(Box<Expr>),
+}
+
+#[derive(Debug)]
+pub enum Pattern {
+    Wildcard,
+    Identifier(String),
+    Number(u64),
+    String(String),
+    Application(String, Vec<Pattern>),
+}
+
+#[derive(Debug)]
+pub struct Signature {
+    pub name: String,
+    pub params: Vec<Type>,
+    pub return_type: Type,
+}
+
+pub struct Constructor {
+    pub name: String,
+    pub types: Vec<Type>,
+}
+
+pub struct Data {
+    pub name: String,
+    pub params: Vec<String>,
+    pub ctors: Vec<Constructor>,
+}
+
+#[derive(Debug)]
+pub struct Fn {
+    pub name: String,
+    pub params: Vec<Pattern>,
+    pub body: FnBody,
 }
 
 #[derive(Debug, PartialEq, Eq)]
