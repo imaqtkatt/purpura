756b6c3792131795da8c325e3be779f54e07c7b7
imaqtkatt <eduarda.so@proton.me>
2023-06-26 11:57:09 -0300

    location: derive debug for structs

--- purpura/location/src/lib.rs
+++ purpura/location/src/lib.rs
@@ -2,15 +2,18 @@
 //! other data structures.
 
 /// The byte position
+#[derive(Debug)]
 pub struct Byte(pub usize);
 
 /// A range between two bytes
+#[derive(Debug)]
 pub struct Location {
     pub start: Byte,
     pub end: Byte,
 }
 
 /// A value with a location
+#[derive(Debug)]
 pub struct Spanned<T> {
     pub value: T,
     pub location: Location,
